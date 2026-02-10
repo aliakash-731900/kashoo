5f4d263adb6b9a3ce96794203f9881069dcc7e47
Muneeb Hussain <muneeb@thefivestarschool.org>
2026-02-10 13:32:42 +0500

    first lisson 123

---
+++ ysk.py
@@ -0,0 +1,17 @@
+number=int(input("enter a number"))
+if number>5:
+    print("no is greater then 5")
+if number>15:
+     print("greater then 15")
+else:
+    print("no is less then 5")
+
+i=20
+if(i==10):
+    print(i)
+elif(i==15):
+    print(i)
+elif(i==20):
+    print(i)
+else:
+    print("no data found")
