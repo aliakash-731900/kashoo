f9e10f438b6f5f7cf31faa3d194f8742464b3ee4
Muneeb Hussain <muneeb@thefivestarschool.org>
2026-02-10 13:29:31 +0500

    hay i'm here

---
+++ it's a equal a==b.py
@@ -0,0 +1,27 @@
+a=5
+print(a)
+a==6
+print(a==6)
+
+i=2
+i>5
+print(i>5)
+#is statement example
+age = 19
+#age = 18
+
+#expression than can be true or false 
+if age>18:
+    #within an indent,we have the expression that is run is the condition is true
+    print("you can enter")
+
+#the statement after the if statememt will run regaldess if the condition is true or false
+print("move on ")
+
+album_year = 1980
+
+if(album_year>1079) and (album_year<1990):
+    print("album_year was in between 1980 and 1989")
+
+print("")
+print("Go fuck your self...")
